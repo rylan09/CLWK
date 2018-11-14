824d9fd003b3b57a1de9ca93794ca712ca3a8ac7
Rylan <curammeng808@gmail.com>
2018-11-13 20:25:28 -0700

    first commit

---
+++ src/HWC1.java
@@ -0,0 +1,47 @@
+
+public class HWC1 {
+
+	public static void main(String[] args) {
+		double numd = 120.00d;
+		double numf = 10d;
+		char achar = 'h';
+		long along = 123222L;
+		boolean isHot = false;
+		
+		short ashort = 123;
+		int aint = -123;
+		float pointX = 12.0f;
+		
+		String name = "kirs";
+		byte abyte = 0;
+		
+		// = assignments of value
+		
+		// + add value
+		double sum = numd + numf;
+		double sum2 = 10 + 7;
+		
+		// * multiplication gets product
+		double product = 100 * 7;
+		
+		// ( / ) Division gets quotients
+		float quoatient = 10.1f / 2f;
+		
+		// % Modulous gets the remainder of the quotient
+		
+		int remainder = 10 % 2; // ==5
+		
+		int number = 1;
+		number++;
+		number = number + 1;
+		
+		System.out.println(name);
+		System.out.println(numd + 5);
+	
+		
+		
+		
+		
+	}
+
+}
---
+++ src/PrimitiveVariables.java
@@ -0,0 +1,41 @@
+
+public class PrimitiveVariables {
+
+	public static void main(String[] args) {
+		//variable1
+		int year = 12;
+		float minute = (float) 99.91;
+		double hours = 97876.954;
+		long size = 947238902;
+		short sizeUnit = 99;
+		byte categoryUnit = 1;
+		char category = 'A';
+		boolean isNumberOne = false;
+		String nameSite = "Netflix";
+		
+		System.out.println(year);
+		System.out.println(minute);
+		System.out.println(hours);
+		System.out.println(size);
+		System.out.println(categoryUnit);
+		System.out.println(category);
+		System.out.println(isNumberOne);
+		System.out.println(nameSite);
+		
+		//variable2
+		int dogsAge = 7;
+		double weight = 78.35;
+		char breedClass = 'K';
+		boolean isHeMean = false;
+		String dogsName = "Rocco";
+		
+		System.out.println(dogsAge);
+		System.out.println(weight);
+		System.out.println(breedClass);
+		System.out.println(isHeMean);
+		System.out.println(dogsName);
+		
+
+	}
+
+}
